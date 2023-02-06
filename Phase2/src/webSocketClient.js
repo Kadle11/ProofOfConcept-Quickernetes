f1b0531921e587db5521575186096525810b7def
vrao79 <vrao79@localhost>
2023-02-06 16:24:45 -0700

    Web Sockets, No OTEL

---
+++ Phase2/src/webSocketClient.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const EventEmitter = require('node:events');
+const process = require('process')
+const opentelemetry = require("@opentelemetry/api");
+const WebSocket = require('ws');
+
+const SERVER_ADDR = process.argv[2]
+const tracer = require('./trace')(('grpc-tunnel-client'));
+
+const app = express();
+app.use(express.json());
+
+const ws = new WebSocket('ws://' + SERVER_ADDR, {
+    perMessageDeflate: false
+});
+
+ws.on('error', console.error);
+
+app.get("/student/:roll", async (req, res) => {
+    // console.log("Recieved request from client for student", req.params.roll)
+
+    // const span = tracer.startSpan('client.js:GET()');
+    // const requestCtx = opentelemetry.trace.setSpan(opentelemetry.context.active(), span)
+    // opentelemetry.context.with(requestCtx, () => {
+    // })
+
+    ws.send(req.params.roll)
+
+    ws.on('message', async function (data) {
+
+        let student = JSON.parse(data)
+        if (student.roll != req.params.roll) {
+            console.log("Need this check!")
+        }
+        res.json(student).send()
+    })
+
+})
+
+app.post("/student", async (req, res) => {
+
+    var student = {};
+    student.name = req.body.name;
+    student.registration = req.body.registration;
+    student.roll = req.body.roll;
+
+})
+
+var server = app.listen(4000, "0.0.0.0", function (err) {
+    if (err) {
+        console.log("[Error] Unable to start server.");
+    }
+    console.log("Started Server on 3000");
+})
