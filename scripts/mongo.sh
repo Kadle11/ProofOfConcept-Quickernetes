e27da269a6d209dcef3fcec968db3fa171a8852f
Vijay Thurimella <vthurimella@Vijays-MacBook-Pro.local>
2023-04-02 21:37:51 -0400

    install mongo script

---
+++ scripts/mongo.sh
@@ -0,0 +1,8 @@
+# Install mongo using apt
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt update
+apt install mongodb-org
+
+# Start mongo
+systemctl start mongod.service
